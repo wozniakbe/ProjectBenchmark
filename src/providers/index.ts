b2ec7e3f73c58214c1767eb3a8773619b24f8dcd
Ben Wozniak <wozniakbe@msoe.edu>
2019-02-05 19:34:19 -0600

    Base ionic 4 side menu application

--- src/providers/index.ts
+++
@@ -1,5 +0,0 @@
-export { Api } from './api/api';
-export { Items } from '../mocks/providers/items';
-export { StocksService } from './stocks/stocks.service';
-export { Settings } from './settings/settings';
-export { User } from './user/user';
